f8041be4eea8fb8e136ba50b1b4acd53d061c108
Isabela de Oliveira <isabela_oliveira3@estudante.sesisenai.org.br>
2026-08-18 19:01:31 -0300

    refactor - remover o campo de senha do cadastro de usuário do banco

--- database/db.sql
+++ database/db.sql
@@ -4,8 +4,7 @@ USE cadastro_pratos;
 CREATE TABLE usuarios (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    senha VARCHAR(100) NOT NULL
+    email VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE pratos (
