60a84fbce71768f663a3b50892300604b7c51151
Isabela <isabela_oliveira3@estudante.sesisenai.org.br>
2026-08-18 10:09:41 -0300

    add - banco de dados

---
+++ database/db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE cadastro_pratos;
+USE cadastro_pratos;
+
+CREATE TABLE usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE pratos (
+    id_prato INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao VARCHAR(200) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    id_usuario INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
+);
